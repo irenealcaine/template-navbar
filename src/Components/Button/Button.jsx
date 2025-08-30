54907d1f1f388333ada63ce82f27db11decf5dda
Irene Alcaine <irenealcainealvarez@gmail.com>
2025-08-30 17:11:16 +0200

    update button with href

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,15 +2,27 @@ import "./Button.css";
 import { DarkModeContext } from "../../Context/darkModeContext";
 import { useContext } from "react";
 
-const Button = ({ value, onClick, className, color, disabled }) => {
+const Button = ({ value, onClick, className, color, disabled, href }) => {
   const { darkMode } = useContext(DarkModeContext);
+
+  if (href && !disabled) {
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={`button ${color} ${className} ${darkMode ? "dark" : ""}`}
+      >
+        {value}
+      </a>
+    );
+  }
 
   return (
     <button
       onClick={onClick}
-      className={`button ${!disabled ? color : "disabled"} ${className} ${
-        darkMode ? "dark" : ""
-      }`}
+      className={`button ${!disabled ? color : "disabled"} ${className} ${darkMode ? "dark" : ""
+        }`}
       disabled={disabled}
     >
       {value}
