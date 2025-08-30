2824002025017077a611b33a402fdd3b8c1aa8ed
Irene Alcaine <irenealcainealvarez@gmail.com>
2025-08-30 16:54:19 +0200

    added sidebar

--- src/Utils/Constants.jsx
+++ src/Utils/Constants.jsx
@@ -1,15 +1,25 @@
+import { PiAddressBookBold, PiAlarmBold } from "react-icons/pi";
+import { CgDatabase, CgTrello } from "react-icons/cg";
+
 export const navbarItems = [
   {
-    "name": "Page 1",
-    "slug": "page1"
-  }, {
-    "name": "Page 2",
-    "slug": "page2"
-  }, {
-    "name": "Page 3",
-    "slug": "page3"
-  }, {
-    "name": "Page 4",
-    "slug": "page4"
+    name: "Page 1",
+    slug: "page1",
+    icon: <PiAddressBookBold />,
   },
-]
+  {
+    name: "Page 2",
+    slug: "page2",
+    icon: <PiAlarmBold />,
+  },
+  {
+    name: "Page 3",
+    slug: "page3",
+    icon: <CgDatabase />,
+  },
+  {
+    name: "Page 4",
+    slug: "page4",
+    icon: <CgTrello />,
+  },
+];
