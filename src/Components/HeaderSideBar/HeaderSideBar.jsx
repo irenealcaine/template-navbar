07e8043dffc0d273fc35527dfcccad6e4046e2f3
Irene Alcaine <irenealcainealvarez@gmail.com>
2025-08-30 16:46:58 +0200

    added sidebar

---
+++ src/Components/HeaderSideBar/HeaderSideBar.jsx
@@ -0,0 +1,87 @@
+import { useContext } from "react";
+import "./HeaderSideBar.css";
+import logo from "../../Assets/Images/logo.jpg";
+import { Link, useLocation } from "react-router-dom";
+import { navbarItems } from "../../Utils/Constants";
+import { useState } from "react";
+import { BiSolidRightArrow } from "react-icons/bi";
+import { DarkModeContext } from "../../Context/darkModeContext";
+
+const HeaderSideBar = () => {
+  const location = useLocation();
+  const [isMenuClosed, setIsMenuClosed] = useState(true);
+  const menuActiveClass = isMenuClosed ? "closed" : "";
+  const { darkMode, dispatch } = useContext(DarkModeContext);
+
+  const toggleMenu = () => {
+    setIsMenuClosed(!isMenuClosed);
+  };
+
+  return (
+    <header className={`${darkMode ? "dark" : ""} ${menuActiveClass}`}>
+      <nav>
+        <Link
+          to={"/"}
+          onClick={() => setIsMenuClosed(true)}
+          className="logo-link"
+        >
+          <img
+            src={logo}
+            alt={"Logo"}
+            className={`logo ${darkMode ? "dark" : ""}`}
+          />
+        </Link>
+
+        <div
+          className={`menu-toggle ${menuActiveClass} ${darkMode ? "dark" : ""}`}
+          onClick={toggleMenu}
+        >
+          <BiSolidRightArrow />
+        </div>
+
+        <ul
+          className={`navigation ${menuActiveClass} ${darkMode ? "dark" : ""}`}
+        >
+          {navbarItems.map((navbarItem) => (
+            <li key={navbarItem.slug} onClick={() => setIsMenuClosed(true)}>
+              <Link
+                to={`/${navbarItem.slug}`}
+                className={`navigation-item  ${darkMode ? "dark" : ""} ${location.pathname === `/${navbarItem.slug}` && "active"
+                  }`}
+              >
+                <span className={`navigation-item-name ${menuActiveClass}`}>
+                  {navbarItem.name}
+                </span>
+                <span className={`navigation-item-icon`}>
+                  {navbarItem.icon}
+                </span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className={`color-options ${menuActiveClass}`}>
+        <div
+          className="color-option"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        >
+          ⚪
+        </div>
+        <div
+          className="color-option"
+          onClick={() => dispatch({ type: "DARK" })}
+        >
+          ⚫
+        </div>
+        <div
+          className="color-option"
+          onClick={() => dispatch({ type: "TOGGLE" })}
+        >
+          {darkMode ? "☀️" : "🌙"}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderSideBar;
--- src/Layout/Main/Main.jsx
+++ src/Layout/Main/Main.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import Footer from "../../Components/Footer/Footer";
+import Footer from "../../Components/Footer/Footer"; //solo con navbar
 import Header from "../../Components/Header/Header";
 import "./Main.css";
 import { DarkModeContext } from "../../Context/darkModeContext";
@@ -11,7 +11,7 @@ const Main = ({ children }) => {
     <div className={`main ${darkMode ? "dark" : ""}`}>
       <Header />
       <div className="content">{children}</div>
-      <Footer />
+      <Footer /> {/* solo con navbar */}
     </div>
   );
 };
